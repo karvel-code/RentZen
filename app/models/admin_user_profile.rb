458e1a82688c971f91928c0f27ec1127e6031bbb
karvel-code <velgithinji@gmail.com>
2024-05-17 14:58:02 +0300

    Add Admin user model and profile

---
+++ app/models/admin_user_profile.rb
@@ -0,0 +1,2 @@
+class AdminUserProfile < ApplicationRecord
+end
---
+++ db/migrate/20240517114937_create_admin_user_profiles.rb
@@ -0,0 +1,12 @@
+class CreateAdminUserProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admin_user_profiles do |t|
+      t.string :first_name, null: false
+      t.string :surname, null: false
+      t.string :phone
+      t.references :admin_user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
