4a9164a22590a8a046a7f8aac5ba1ef52f09faab
karvel-code <velgithinji@gmail.com>
2024-07-30 20:23:36 +0300

    Add unit type model

---
+++ test/models/unit_type_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: unit_types
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require "test_helper"
+
+class UnitTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
