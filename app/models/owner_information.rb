2160878436d4779b22c6bc2fdce119ed759e6a8c
karvel-code <velgithinji@gmail.com>
2024-08-01 19:39:31 +0300

    Make some corrections

--- app/models/owner_information.rb
+++ app/models/owner_information.rb
@@ -24,12 +24,6 @@
 class OwnerInformation < ApplicationRecord
   belongs_to :unit_owner
   
-  enum status: {
-    invited: 'invited',
-    current: 'current',
-    past: 'past'
-  }
-
   class << self
     def current_unit_info(unit_id, unit_owner_id)
       self.find_by(unit_id: unit_id, unit_owner_id: unit_owner_id)
